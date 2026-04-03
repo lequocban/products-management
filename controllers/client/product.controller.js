495230d4a58a279183869ae21ad620ae935439b9
lequocban <lequocban2005@gmail.com>
2026-04-03 18:37:22 +0700

    hiển thị danh sách sản phẩm bên client theo danh mục

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const productsHelper = require("../../helper/product");
+const productsCategoryHelper = require("../../helper/productCategory");
 
 // [GET]  /products
 module.exports.index = async (req, res) => {
@@ -17,11 +19,11 @@ module.exports.index = async (req, res) => {
   });
 };
 
-// [GET]  /products/:slug
+// [GET]  /products/detail/:slugProduct
 module.exports.detail = async (req, res) => {
   try {
     const product = await Product.findOne({
-      slug: req.params.slug,
+      slug: req.params.slugProduct,
       deleted: false,
       status: "active",
     });
@@ -34,6 +36,41 @@ module.exports.detail = async (req, res) => {
     res.render("client/pages/products/detail", {
       pageTitle: product.title,
       product: product,
+    });
+  } catch (error) {
+    res.redirect(req.headers.referer);
+  }
+};
+
+// [GET]  /products/:slugCategory
+module.exports.category = async (req, res) => {
+  try {
+    const category = await ProductCategory.findOne({
+      slug: req.params.slugCategory,
+      status: "active",
+      deleted: false,
+    });
+
+    if (!category) {
+      return res.redirect("/");
+    }
+
+    const listSubCategory = await productsCategoryHelper.getSubCategory(
+      category.id,
+    );
+    const listSubCategoryId = listSubCategory.map((item) => item.id);
+    const listCategoryId = [category.id, ...listSubCategoryId];
+    const products = await Product.find({
+      product_category_id: { $in: listCategoryId },
+      status: "active",
+      deleted: false,
+    }).sort({ position: "desc" });
+
+    const newProducts = productsHelper.priceNewProducts(products);
+
+    res.render("client/pages/products/index", {
+      pageTitle: category.title,
+      products: newProducts,
     });
   } catch (error) {
     res.redirect(req.headers.referer);
--- routes/client/products.route.js
+++ routes/client/products.route.js
@@ -5,6 +5,8 @@ const controllers = require("../../controllers/client/product.controller")
 
 router.get("/", controllers.index);
 
-router.get("/:slug", controllers.detail);
+router.get("/:slugCategory", controllers.category);
+
+router.get("/detail/:slugProduct", controllers.detail);
 
 module.exports = router;
