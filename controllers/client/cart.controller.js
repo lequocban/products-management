3803758d7430ed30cdc7ec922b830bac8cb80e83
lequocban <lequocban2005@gmail.com>
2026-04-04 22:24:49 +0700

    trang giỏ hàng bên client

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,5 +1,29 @@
 const Cart = require("../../models/cart.model");
 
+const productsHelper = require("../../helper/product");
+
+// [GET]  /cart
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const cart = await Cart.findOne({ _id: cartId }).populate(
+    "products.product_id",
+  );
+
+  for (const item of cart.products) {
+    item.product_id.newPrice = productsHelper.priceNewProduct(item.product_id);
+    item.totalPrice = (item.product_id.newPrice * item.quantity).toFixed(2);
+  }
+
+  cart.totalPrice = cart.products.reduce((total, item) => {
+    return total + parseFloat(item.totalPrice);
+  }, 0);
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    cart: cart,
+  });
+};
+
 // [POST]  /cart/add/:productId
 module.exports.addPost = async (req, res) => {
   const cartId = req.cookies.cartId;
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const controllers = require("../../controllers/client/cart.controller");
 
+router.get("/", controllers.index);
+
 router.post("/add/:productId", controllers.addPost);
 
 module.exports = router;
