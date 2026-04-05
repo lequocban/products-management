6fff9cb6191ac20c7e6202be1082b1622073b470
lequocban <lequocban2005@gmail.com>
2026-04-05 18:10:22 +0700

    create collection and model order

---
+++ models/order.model.js
@@ -0,0 +1,33 @@
+const { default: mongoose } = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    // user_id: String,
+    cart_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cart",
+    },
+    user_info: {
+      fullName: String,
+      phone: String,
+      address: String,
+    },
+    products: [
+      {
+        product_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        price: Number,
+        discountPercentage: Number,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+const Order = mongoose.model("Order", orderSchema, "orders");
+
+module.exports = Order;
