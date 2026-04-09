57ef0e2d5d834462082577ba61f94526cfa221bd
lequocban <lequocban2005@gmail.com>
2026-04-09 14:26:11 +0700

    create collection, model chat

---
+++ models/chat.model.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    rom_chat_id: String,
+    content: String,
+    image: Array,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+const Chat = mongoose.model("Chat", chatSchema, "chats");
+
+module.exports = Chat;
