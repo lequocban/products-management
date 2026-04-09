213571bbba888bd3cb9a0f097687aef7a06f16e0
lequocban <lequocban2005@gmail.com>
2026-04-09 15:18:59 +0700

    basic chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,11 +1,30 @@
+const Chat = require("../../models/chat.model");
+
 // [GET] /chat
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
+
   // socket io
-  _io.on("connection", (socket) => {
+  _io.once("connection", (socket) => {
     console.log("a user connected: ", socket.id);
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
   // end socket io
+
+  // lấy data
+  const chats = await Chat.find({
+    deleted: false,
+  }).populate("user_id", "fullName");
+
+  // end lấy data
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,17 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if (formSendData) {
+  const input = document.getElementById("input");
+
+  formSendData.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const content = e.target.elements.content.value;
+    if (content.trim() !== "") {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      e.target.elements.content.value = "";
+    }
+  });
+}
+
+// END_CLIENT_SEND_MESSAGE
