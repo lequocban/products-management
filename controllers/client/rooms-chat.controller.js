4a8795b0ad2cb16607b08725017f89477ae137e1
lequocban <lequocban2005@gmail.com>
2026-04-17 13:17:20 +0700

    update interface

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -10,15 +10,50 @@ module.exports.index = async (req, res) => {
     typeRoom: "group",
     deleted: false,
   });
+
+  const currentUser = await User.findOne({ _id: userId })
+    .select("friendList")
+    .lean();
+
+  const friendList = currentUser?.friendList || [];
+  const friendIds = friendList.map((item) => item.user_id);
+
+  const listPrivateChatUsers = await User.find({
+    _id: { $in: friendIds },
+    deleted: false,
+    status: "active",
+  })
+    .select("id fullName avatar statusOnline")
+    .lean();
+
+  const listDirectChat = listPrivateChatUsers
+    .map((friendUser) => {
+      const friendInfo = friendList.find(
+        (friend) => String(friend.user_id) === String(friendUser._id),
+      );
+
+      return {
+        id: friendUser.id,
+        fullName: friendUser.fullName,
+        avatar: friendUser.avatar,
+        statusOnline: friendUser.statusOnline,
+        room_chat_id: friendInfo?.room_chat_id,
+      };
+    })
+    .filter((item) => item.room_chat_id);
+
   res.render("client/pages/rooms-chat/index", {
     pageTitle: "Danh sách phòng chat",
     listRoomChat: listRoomChat,
+    listDirectChat: listDirectChat,
   });
 };
 
 // [GET] /rooms-chat/create
 module.exports.create = async (req, res) => {
-  const friendsList = res.locals.user.friendList || [];
+  const friendsList = res.locals.user.friendList
+    ? res.locals.user.friendList.toObject()
+    : [];
 
   for (const friend of friendsList) {
     const infoFriend = await User.findOne({
@@ -37,7 +72,7 @@ module.exports.create = async (req, res) => {
 // [POST] /rooms-chat/create
 module.exports.createPost = async (req, res) => {
   const title = req.body.title;
-  const usersId = req.body.usersId;
+  const usersId = [].concat(req.body.usersId || []);
 
   const dataChat = {
     title: title,
